3be34203c11d55e0a6a2269a930716a20b506b82
c-ch6_9273c <diffh14@gmail.com>
2023-07-16 20:10:02 +0700

    gua-bikin-register-page-dan-login-page

--- client/src/Router/index.js
+++ client/src/Router/index.js
@@ -1,16 +1,19 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-
 // Pages
 import LandingPage from "../Pages/LandingPage";
 import ProfilePage from "../Pages/ProfilePage";
+import RegisterPage from "../Pages/RegisterPage";
+import LoginPage from "../Pages/LoginPage";
 
 function Router() {
     return (
         <Routes>
             <Route path="/" Component={LandingPage}/>
             <Route path="/ProfilePage" Component={ProfilePage} />
+            <Route path="/registerPage" component={RegisterPage} />
+            <Route path="/loginPage" component={LoginPage} />
         </Routes>
     )
 }
